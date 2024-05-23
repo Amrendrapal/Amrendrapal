1fb94befbf22c1c747282b4ddfdb436dd410284f
Amrendrapal <147226666+Amrendrapal@users.noreply.github.com>
2024-05-24 01:46:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+const cardsArray = [
+    'A', 'A', 'B', 'B',
+    'C', 'C', 'D', 'D',
+    'E', 'E', 'F', 'F',
+    'G', 'G', 'H', 'H'
+];
+
+let gameBoard = document.getElementById('game-board');
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+let matchedCards = 0;
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function createBoard() {
+    const shuffledCards = shuffle(cardsArray);
+    shuffledCards.forEach((letter) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.letter = letter;
+        card.addEventListener('click', flipCard);
+        gameBoard.appendChild(card);
+    });
+}
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('hidden');
+    this.textContent = this.dataset.letter;
+
+    if (!firstCard) {
+        firstCard = this;
+    } else {
+        secondCard = this;
+        lockBoard = true;
+        checkForMatch();
+    }
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.letter === secondCard.dataset.letter) {
+        disableCards();
+        matchedCards += 2;
+        if (matchedCards === cardsArray.length) {
+            setTimeout(() => alert('Congratulations! You have matched all cards!'), 500);
+        }
+    } else {
+        unflipCards();
+    }
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    firstCard.classList.add('matched');
+    secondCard.classList.add('matched');
+    resetBoard();
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('hidden');
+        secondCard.classList.remove('hidden');
+        firstCard.textContent = '';
+        secondCard.textContent = '';
+        resetBoard();
+    }, 1000);
+}
+
+function resetBoard() {
+    [firstCard, secondCard, lockBoard] = [null, null, false];
+}
+
+createBoard();
